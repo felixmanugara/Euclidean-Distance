408677103ab7211d19fb58e7a18e986c0226c776
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-20 20:09:50 +0700

    change: add argument to each function

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -9,39 +9,36 @@ def table():
    
     return data
 
-df = table()
 
-def koordinat_GPS():
+def koordinat_GPS(df):
     np_gps_arr = pd.DataFrame(df).iloc[:,[0,1]].to_numpy(dtype=np.float64)
     np_ref_arr = np.array([-5.3723438,105.2500923]) 
     #print(np_ref_arr)
     
     return np_gps_arr, np_ref_arr
     
-np_gps_arr, np_ref_arr = koordinat_GPS()
+np_gps_arr, np_ref_arr = koordinat_GPS(table())
 
 
 # euclidean distance formula
-def counting_process():
-    formula = np.linalg.norm(np_gps_arr - np_ref_arr, axis=1)    
-    return formula
-
-formula = counting_process()
+def counting_process(gps_data,ref_data):
+    data_count = np.linalg.norm(gps_data - ref_data, axis=1)    
+    return data_count
 
 # loop for storing calculation result
-def store_data():
+def store_data(data_counted):
     new_arr = []
-    for i in formula:
+    for i in data_counted:
         result = i * 111.322
         km = result * 1000
         m = round(km,2)
         new_arr.append(m)
 
     np_new_arr = np.array(new_arr)
-    print(np_new_arr)
+    #print(np_new_arr)
     return np_new_arr
 
-data_stored = store_data()
+data_stored = store_data(counting_process(np_gps_arr, np_ref_arr))
 
 average = np.mean(data_stored)
 average_value = (f"jarak error rata-rata adalah: {average:.2f} Meter")
