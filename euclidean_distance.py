f4b81246f7a0c0ff6b077b43337ed727725d8d84
Felix Manugara <marcofel.manugara@gmail.com>
2021-09-24 16:15:25 +0700

    add: dataframe for viewing data

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,41 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
-
-# koordinat_mod from gps module reading
-def koordinat_GPS():
-    gps_data =[[-5.3741278,105.2514637],
-               [-5.3727553,105.2522518],
-               [-5.3732140,105.2508992],
-               [-5.3704840,105.2496747],
-               [-5.3728913,105.2455960],
-               [-5.3635015,105.2456365],
-               [-5.3656357,105.2443033]]
-    
-    return gps_data
-    
-# koordinat_ref being use for calculate error distance
-def koordinat_referensi():
-    data_ref = [[-5.3741387,105.2514740],
-                [-5.3727958,105.2522109],
-                [-5.3732511,105.2508500],
-                [-5.3702449,105.2497647],
-                [-5.3729377,105.2456338],
-                [-5.3634703,105.2455758],
-                [-5.3657855,105.2442934]]
-    return data_ref
-
-gps_data = koordinat_GPS()
-data_ref = koordinat_referensi()
+import pandas as pd
 
 # numpy array
-def array(gps_array,ref_array):
-    np_gps_arr = np.array(gps_array)
-    np_ref_arr = np.array(ref_array)
+def array():
+    np_gps_arr = np.array([[-5.3741278,105.2514637],
+                           [-5.3727553,105.2522518],
+                           [-5.3732140,105.2508992],
+                           [-5.3704840,105.2496747],
+                           [-5.3728913,105.2455960],
+                           [-5.3635015,105.2456365],
+                           [-5.3656357,105.2443033]])
+
+    np_ref_arr = np.array([[-5.3741387,105.2514740],
+                           [-5.3727958,105.2522109],
+                           [-5.3732511,105.2508500],
+                           [-5.3702449,105.2497647],
+                           [-5.3729377,105.2456338],
+                           [-5.3634703,105.2455758],
+                           [-5.3657855,105.2442934]])
     
     return np_gps_arr, np_ref_arr
 
-np_gps_arr, np_ref_arr = array(gps_data,data_ref)
+np_gps_arr, np_ref_arr = array()
+
 
 # euclidean distance formula
 def counting_process():
@@ -54,13 +42,22 @@ def store_data():
         new_arr.append(m)
 
     np_new_arr = np.array(new_arr)
-    print(np_new_arr)
+    #print(np_new_arr)
     return np_new_arr
 
 data_stored = store_data()
 
 average = np.mean(data_stored)
-print(f"jarak error rata-rata adalah: {average:.2f} Meter")
+#print(f"jarak error rata-rata adalah: {average:.2f} Meter")
+
+def dataframe(gps_reff, gps_arr):
+    df_reff = pd.DataFrame(gps_reff, columns=['Latitude (ref)', 'Longitude (ref)'], dtype=np.float64)
+    df_gps = pd.DataFrame(gps_arr, columns=['Latitude (GPS)', 'Longitude (GPS)'], dtype=np.float64)
+    df_all = pd.concat([df_reff,df_gps], axis= 1, join='outer')
+    df_all['selisih Jarak (meter)'] = data_stored
+    print(df_all)
+
+dataframe(np_ref_arr, np_gps_arr)
 
 def data_plot(source):
     bins = [1.5, 3.5, 5.5, 7.5, 9.5, 11.5, 13.5, 15.5, 17.5]
@@ -79,4 +76,4 @@ def data_plot(source):
     plt.legend(loc="best")
     plt.show()
 
-data_plot(data_stored)
+#data_plot(data_stored)
