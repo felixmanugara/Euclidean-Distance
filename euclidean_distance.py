63232ee53f0cdaba0920af390f0534400d72b530
Felix Manugara <marcofel.manugara@gmail.com>
2021-12-06 17:58:07 +0700

    add: csv dataset

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import math
+from scipy import stats
 
 
 def table():
     # create dataframe from .csv
-    data = pd.read_csv('gpsdat.csv',engine='python')
-    #print(data)
-    return data
+    data = pd.read_csv('gpsdat.csv',engine='python', parse_dates=['Dates'])
+    dataset = pd.DataFrame(data, index= range(1,len(data)))
+    # cleaning data from duplicate values
+    dataset = dataset.drop_duplicates(subset=['Latitude modul','Longitude modul','Sat count','Speed','Alt'],keep=False, inplace= False)
+    dataset.index = range(1, len(dataset)+1)
+    
+    return dataset
 
+dataset = table()
 
 def koordinat_GPS(df):
-    np_gps_arr = pd.DataFrame(df).loc[:,['Latitude','Longitude']].to_numpy(dtype=np.float32)
+    np_gps_arr = pd.DataFrame(df).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
     #print(np_gps_arr)
-    np_ref_arr = np.array([-5.3723438,105.2500923]) 
+    np_ref_arr = np.array([-5.3721512,105.2500960]) 
     #print(np_ref_arr)
     
     return np_gps_arr, np_ref_arr
@@ -41,12 +48,27 @@ def store_data(data_counted):
 
 data_stored = store_data(counting_process(np_gps_arr, np_ref_arr))
 
-average = np.mean(data_stored)
-average_value = (f"jarak error rata-rata adalah: {average:.2f} Meter")
-print(average_value)
+average = stats.mode(data_stored)[0] # mencari nilai rata-rata
+#average_value = (f"jarak error rata-rata adalah: {average} Meter")
+#print(average_value)
+
+def dataset():
+    reference_coordinate = pd.DataFrame(np_ref_arr.reshape(1,2),columns=['Latitude ref','Longitude ref'])
+    reference_coordinate.index = range(1,len(reference_coordinate)+1) # index using data length
+    error_distance_data = pd.DataFrame(np.array(data_stored),columns=['Error Distance (m)'],index=range(1,len(data_stored)+1))
+    merge_data = pd.concat([reference_coordinate,table(),error_distance_data],axis=1)
+    df = merge_data.fillna(method='ffill')
+    df.drop(columns='Dates')
+   #df.head(10)
+
+    return error_distance_data, df
+
+error_distance_data, df = dataset()
 
 def data_plot(source):
-    bins = 20
+    min_val = error_distance_data.min()
+    max_val = error_distance_data.max()
+    bins = math.ceil((max_val - min_val) / 3)
     xlab = 'Nilai Error Dalam Meter'
     ylab = 'Jumlah Data'
     color = '#ff7f50'
@@ -58,7 +80,9 @@ def data_plot(source):
 
     plt.hist(source,bins=bins,edgecolor="black", alpha=0.8)
     plt.axvline(average,color=color,label='Nilai rata-rata',linewidth=2)
+    plt.grid(color='grey',alpha=0.4)
     plt.legend(loc='best')
     plt.show()
                         
 data_plot(data_stored) 
+print(df)
