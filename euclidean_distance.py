54f9bc3866b116aafe8013bcc2c578558f12e912
Felix Manugara <marcofel.manugara@gmail.com>
2022-02-06 15:37:29 +0700

    fix: paramater and variable name convention

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -16,12 +16,12 @@ class GpsDataAnalytics:
 
 
     def extractData(self,dataFrame):
-        self.ModuleArray = pd.DataFrame(dataFrame).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
+        self.moduleArray = pd.DataFrame(dataFrame).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
         self.referenceArray = np.array([-5.3721512,105.2500960])
         #print(self.npGpsModuleArray)
         #print(self.npGpsReferenceArray)
         #method call
-        self.countingProcess(self.ModuleArray,self.referenceArray)
+        self.countingProcess(self.moduleArray,self.referenceArray)
 
     def countingProcess(self,gpsModuleArray, gpsReferenceArray):
         self.dataProc = np.linalg.norm(gpsModuleArray - gpsReferenceArray , axis=1)
@@ -29,24 +29,24 @@ class GpsDataAnalytics:
         self.dataResult(self.dataProc)
 
     def dataResult(self, processedData):
-        resultArray = []
+        _resultArray = []
         for i in processedData:
-            multiplication = i * 111.322
-            km = multiplication * 1000
-            m = round(km, 2)
-            resultArray.append(round(m))
+            _multiplication = i * 111.322
+            _km = _multiplication * 1000
+            _m = round(_km, 2)
+            _resultArray.append(round(_m))
 
-        self.resultArrayData = np.array(resultArray)
+        self.resultArrayData = np.array(_resultArray)
         #print(self.resultArrayData)
         self.finalDataTable(self.resultArrayData,self.referenceArray,self.dataset)
         
     
-    def finalDataTable(self, errorDistanceData, gpsReferenceData, Dataframe):
+    def finalDataTable(self, errorDistanceData, gpsReferenceData, dataFrame):
         referenceCoordinate = pd.DataFrame(gpsReferenceData.reshape(1,2),columns=['Latitude Referensi','Longitude Referensi'])
         referenceCoordinate.index= range(1,len(referenceCoordinate)+1)
         # index using data length
         self.errorDistanceData = pd.DataFrame(errorDistanceData, columns=['Jarak Error (Meter)'], index= range(1,len(self.resultArrayData)+1))
-        mergeData = pd.concat([referenceCoordinate,Dataframe,self.errorDistanceData], axis=1)
+        mergeData = pd.concat([referenceCoordinate,dataFrame,self.errorDistanceData], axis=1)
         finalDataset = mergeData.fillna(method='ffill') # fill missing value 
         finalDataset.drop(columns=['Sat count','Speed','Dates','Alt'], inplace=True)
         #print(finalDataset)
@@ -56,18 +56,18 @@ class GpsDataAnalytics:
         minVal = min(self.resultArrayData)
         maxVal = max(self.resultArrayData)
         average = np.median(self.resultArrayData)
-        binCount = math.ceil((maxVal - minVal) / 3)
-        xLabel = "Nilai error dalam Meter"
-        yLabel = "Jumlah Data"
+        _binCount = math.ceil((maxVal - minVal) / 3)
+        _xLabel = "Nilai error dalam Meter"
+        _yLabel = "Frekuensi Data"
         color = "#ff7f50"
 
         #plot customizations
         plt.title("Selisih Jarak Error GPS")
-        plt.xlabel(xLabel)
-        plt.ylabel(yLabel)
+        plt.xlabel(_xLabel)
+        plt.ylabel(_yLabel)
         #plt.yticks(np.arange(0,22,2))
 
-        plt.hist(dataValue,bins=binCount,edgecolor="black", alpha=0.8)
+        plt.hist(dataValue,bins=_binCount,edgecolor="black", alpha=0.8)
         plt.axvline(average,color=color,label='Nilai rata-rata',linewidth=2)
         plt.grid(color='grey',alpha=0.4)
         plt.legend(loc='best')
