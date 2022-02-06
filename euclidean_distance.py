cb59f5acef224dd8f2fd4c70c7115b573eb4a32b
Felix Manugara <marcofel.manugara@gmail.com>
2022-02-06 15:30:55 +0700

    changes: remove unnecessary function call

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -5,40 +5,48 @@ import math
 
 class GpsDataAnalytics:
 
-    def __init__(self, GPSData):
-        self.data = pd.read_csv(GPSData, engine='python', parse_dates=['Dates'])
+    def __init__(self, gpsData):
+        self.data = pd.read_csv(gpsData, engine='python', parse_dates=['Dates'])
         self.dataset = pd.DataFrame(self.data, index= range(1,len(self.data)))
         self.dataset.drop_duplicates(subset=['Latitude modul','Longitude modul','Sat count','Speed','Alt'], keep= False,inplace= True)
         self.dataset.index = range(1, len(self.dataset)+1)
+        #method call
+        self.extractData(self.dataset)
         #print(dataset)
 
-    def extractData(self):
-        self.npGpsModuleArray = pd.DataFrame(self.dataset).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
-        self.npGpsReferenceArray = np.array([-5.3721512,105.2500960])
+
+    def extractData(self,dataFrame):
+        self.ModuleArray = pd.DataFrame(dataFrame).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
+        self.referenceArray = np.array([-5.3721512,105.2500960])
         #print(self.npGpsModuleArray)
         #print(self.npGpsReferenceArray)
+        #method call
+        self.countingProcess(self.ModuleArray,self.referenceArray)
 
-    def countingProcess(self):
-        self.dataProc = np.linalg.norm(self.npGpsModuleArray - self.npGpsReferenceArray, axis=1)
-        #print(self.dataProc)
+    def countingProcess(self,gpsModuleArray, gpsReferenceArray):
+        self.dataProc = np.linalg.norm(gpsModuleArray - gpsReferenceArray , axis=1)
+        #method call
+        self.dataResult(self.dataProc)
 
-    def dataResult(self):
+    def dataResult(self, processedData):
         resultArray = []
-        for i in self.dataProc:
+        for i in processedData:
             multiplication = i * 111.322
             km = multiplication * 1000
             m = round(km, 2)
-            resultArray.append(m)
+            resultArray.append(round(m))
 
-        self.resultArrayData = np.array(resultArray, dtype=np.int32)
+        self.resultArrayData = np.array(resultArray)
         #print(self.resultArrayData)
+        self.finalDataTable(self.resultArrayData,self.referenceArray,self.dataset)
+        
     
-    def finalDataTable(self):
-        referenceCoordinate = pd.DataFrame(self.npGpsReferenceArray.reshape(1,2),columns=['Latitude Referensi','Longitude Referensi'])
+    def finalDataTable(self, errorDistanceData, gpsReferenceData, Dataframe):
+        referenceCoordinate = pd.DataFrame(gpsReferenceData.reshape(1,2),columns=['Latitude Referensi','Longitude Referensi'])
         referenceCoordinate.index= range(1,len(referenceCoordinate)+1)
         # index using data length
-        self.errorDistanceData = pd.DataFrame(self.resultArrayData, columns=['Jarak Error (Meter)'], index= range(1,len(self.resultArrayData)+1))
-        mergeData = pd.concat([referenceCoordinate,self.dataset,self.errorDistanceData], axis=1)
+        self.errorDistanceData = pd.DataFrame(errorDistanceData, columns=['Jarak Error (Meter)'], index= range(1,len(self.resultArrayData)+1))
+        mergeData = pd.concat([referenceCoordinate,Dataframe,self.errorDistanceData], axis=1)
         finalDataset = mergeData.fillna(method='ffill') # fill missing value 
         finalDataset.drop(columns=['Sat count','Speed','Dates','Alt'], inplace=True)
         #print(finalDataset)
@@ -57,7 +65,7 @@ class GpsDataAnalytics:
         plt.title("Selisih Jarak Error GPS")
         plt.xlabel(xLabel)
         plt.ylabel(yLabel)
-        plt.yticks(np.arange(0,22,2))
+        #plt.yticks(np.arange(0,22,2))
 
         plt.hist(dataValue,bins=binCount,edgecolor="black", alpha=0.8)
         plt.axvline(average,color=color,label='Nilai rata-rata',linewidth=2)
@@ -65,15 +73,14 @@ class GpsDataAnalytics:
         plt.legend(loc='best')
         plt.show()
     
-    def run(self):
-        self.extractData()
-        self.countingProcess()
-        self.dataResult()
-        self.finalDataTable()
-        self.dataPlot()
-
+    #def run(self):
+        #self.extractData()
+        #self.countingProcess()
+        #self.dataResult()
+        #self.finalDataTable()
+        #self.dataPlot()
 
 
 Analytics = GpsDataAnalytics('gpsdat.csv')
-Analytics.run()
+Analytics.dataPlot()
 
