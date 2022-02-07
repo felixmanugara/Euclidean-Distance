a4380842437abd8fa0aba8e4b7441f5e5ef44a95
Felix Manugara <marcofel.manugara@gmail.com>
2022-02-07 23:49:06 +0700

    changes: remove extractdata method

--- GpsDataAnalytics.py
+++ GpsDataAnalytics.py
@@ -4,25 +4,19 @@ import pandas as pd
 import math
 import tabulate
 
-class ReadDataFromCSV:
+class GpsDatafromsameLocation:
 
     def __init__(self, gpsDataFromCsv):
         self.data = pd.read_csv(gpsDataFromCsv, engine='python', parse_dates=['Dates'], dtype={"Latitude modul": np.float64, "Longitude modul": np.float64})
-        self.dataset = pd.DataFrame(self.data, index= range(1,len(self.data)))
-        self.dataset.drop_duplicates(subset=['Latitude modul','Longitude modul','Sat count','Speed','Alt'], keep= False,inplace= True)
-        self.dataset.index = range(1,40)
-        #method call
-        self.extractData(self.dataset)
-        #print(dataset)
-
-
-    def extractData(self,datasetFromCsv):
-        self.moduleArray = pd.DataFrame(datasetFromCsv).loc[:,['Latitude modul','Longitude modul']].to_numpy(dtype=np.float32)
-        self.referenceArray = np.array([-5.3721512,105.2500960])
-        #print(self.npGpsModuleArray)
-        #print(self.npGpsReferenceArray)
+        #self.dataset = pd.DataFrame(self.data, index= range(1,len(self.data)))
+        self.data.drop_duplicates(subset=['Latitude modul','Longitude modul','Sat count','Speed','Alt'], keep= False,inplace= True)
+        self.data.index = range(1,40)
+        # convert data to numpy array
+        self.moduleArray = self.data.loc[:,['Latitude modul','Longitude modul']].to_numpy()
+        self.referenceArray = np.array([-5.3721512,105.2500960],dtype=np.float64)
         #method call
         self.countingProcess(self.moduleArray,self.referenceArray)
+        #print(self.data)
 
     def countingProcess(self,gpsModuleArray, gpsReferenceArray):
         self.dataProc = np.linalg.norm(gpsModuleArray - gpsReferenceArray , axis=1)
@@ -37,13 +31,12 @@ class ReadDataFromCSV:
         self.dataCounted = datainMeters.astype(np.int32)
 
         #print(self.dataCounted)
-        self.finalDataTable(self.dataCounted,self.referenceArray,self.dataset)
+        self.finalDataTable(self.dataCounted,self.referenceArray,self.data)
         
     
     def finalDataTable(self, errorDistanceData, gpsReferenceData, datasetFromCsv):
-        index = list(range(1,40))
         referenceCoordinate = pd.DataFrame(gpsReferenceData.reshape(1,2),columns=['Latitude Referensi','Longitude Referensi'],index = [1], dtype= np.float64)
-        errorDistance = pd.DataFrame(errorDistanceData, columns=['Jarak Error (Meter)'], index= index)
+        errorDistance = pd.DataFrame(errorDistanceData, columns=['Jarak Error (Meter)'], index= range(1,40))
         mergeData = pd.concat([referenceCoordinate,datasetFromCsv,errorDistance], axis=1)
         finalDataset = mergeData.fillna(method='ffill') # fill missing value 
         finalDataset.drop(columns=['Sat count','Speed','Dates','Alt'], inplace=True)
@@ -72,29 +65,18 @@ class ReadDataFromCSV:
         plt.show()
     
 
-class DataFromList:
-     __gpsModuleData = [[-5.3741278,105.2514637],
-                        [-5.3727553,105.2522518],
-                        [-5.3732140,105.2508992],
-                        [-5.3704840,105.2496747],
-                        [-5.3728913,105.2455960],
-                        [-5.3635015,105.2456365],
-                        [-5.3656357,105.2443033]]
+class GpsDataFromDifferentLocation:
+     
     
-     __gpsReferenceData = [[-5.3741387,105.2514740],
-                           [-5.3727958,105.2522109],
-                           [-5.3732511,105.2508500],
-                           [-5.3702449,105.2497647],
-                           [-5.3729377,105.2456338],
-                           [-5.3634703,105.2455758],
-                           [-5.3657855,105.2442934]]
-    
-     def __init__(self):
-         self.gpsModuleArray = np.array(DataFromList.__gpsModuleData)
-         self.gpsReferenceArray = np.array(DataFromList.__gpsReferenceData)
-
-         self.counting_process(self.gpsModuleArray,self.gpsReferenceArray)
-
+     def __init__(self,gpsDatafromCsv):
+        self.data = pd.read_csv(gpsDatafromCsv,index_col=None, engine='python',dtype={"Latitude modul": np.float64, "Longitude modul": np.float64})
+        self.data.index = range(1,8)
+        # convert data to numpy array
+        self.moduleArray = self.data.loc[:,['Latitude modul','Longitude modul']].to_numpy()
+        self.referenceArray = self.data.loc[:,['Latitude referensi','Longitude referensi']].to_numpy()
+        #print(self.data)
+        #method call
+        self.counting_process(self.moduleArray,self.referenceArray)
 
      def counting_process(self, gpsModuleArray, gpsReferenceArray):
         self.dataProc = np.linalg.norm(gpsModuleArray - gpsReferenceArray, axis=1)
@@ -104,18 +86,14 @@ class DataFromList:
         data = dataProcessed * 111.322
         converttoMeters = data * 1000
         datainMeters = np.around(converttoMeters)
-
         self.dataCounted = datainMeters.astype(np.int32)
         #print(self.dataCounted)
-        self.dataframe(self.gpsModuleArray,self.gpsReferenceArray,self.dataCounted)
+        self.dataframe(self.data,self.dataCounted)
         
         
-     def dataframe(self, gpsModuleArray, gpsReferenceArray, dataCounted):
-        index = list(range(1,8))
-        referenceData = pd.DataFrame(gpsReferenceArray,index= index, columns=['Latitude referensi', 'Longitude referensi'], dtype=np.float64)
-        gpsData = pd.DataFrame(gpsModuleArray, index= index, columns=['Latitude modul', 'Longitude modul'], dtype=np.float64)
-        resultData = pd.DataFrame(dataCounted, index= index, columns=['Jarak Error (Meter)'])
-        self.df = pd.concat([referenceData,gpsData,resultData], axis= 1, join='outer')
+     def dataframe(self, dataFrame, dataCounted):
+        resultData = pd.DataFrame(dataCounted, index=range(1,8), columns=['Jarak Error (Meter)'])
+        self.df = pd.concat([dataFrame,resultData], axis= 1, join='outer')
         print(tabulate.tabulate(self.df, tablefmt='psql', showindex=True, headers='keys'))
 
      
@@ -137,17 +115,16 @@ class DataFromList:
         #plt.legend(loc="best")
         plt.show()
 
-   
 
 def run(InputNumber:int):
     if InputNumber > 2:
         raise "masukkan angka 1 atau 2"
     else:
         if InputNumber == 1:
-            Analytics = ReadDataFromCSV('gpsdat.csv')
+            Analytics = GpsDatafromsameLocation('gpsdat.csv')
             Analytics.dataPlot()
         elif InputNumber == 2:
-            Analytics = DataFromList()
+            Analytics = GpsDataFromDifferentLocation("DifferentLocation.csv")
             Analytics.dataPlot()
 
 run(2)
